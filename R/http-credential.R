e52c58c221819a4dd33de4a1444c9baee6d450c6
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-13 18:34:44 +0100

    Add save option

--- R/http-credential.R
+++ R/http-credential.R
@@ -6,16 +6,25 @@
 #' to prompt the user for credentials and return those.
 #' Use [git_credential_update] to update the credential for a given url.
 #'
+#' The appearance and security policy of the credential store depends
+#' on your the configuration of the version of git, your operating system
+#' and which [credential_helper] is in use.
+#'
 #' @export
 #' @family credentials
 #' @rdname http_credentials
 #' @name http_credentials
 #' @aliases credentials
 #' @param url target url, possibly including username or path
+#' @param save in case the user is prompted for credentials, attempt to
+#' remember them.
 #' @param verbose print errors from `git credetial` to stdout
-git_credential_read <- function(url = "https://github.com", verbose = TRUE){
+git_credential_read <- function(url = "https://github.com", save = TRUE, verbose = TRUE){
   cred <- parse_url(url)
-  credential_fill(cred = cred, verbose = verbose)
+  out <- credential_fill(cred = cred, verbose = verbose)
+  if(isTRUE(save))
+    credential_approve(out, verbose = verbose)
+  out
 }
 
 #' @export
